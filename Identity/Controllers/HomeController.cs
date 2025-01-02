a3d37600fb0201a6d8965a331dd98bb2fc8bfa7d
Youssef Othman <yousef.osman.31@gmail.com>
2025-01-02 11:23:48 +0200

    update login form and client configuration

--- Identity/Controllers/HomeController.cs
+++ Identity/Controllers/HomeController.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Identity.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult About()
         {
             return View();
         }
